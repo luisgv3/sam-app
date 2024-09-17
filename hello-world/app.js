53c41094dc04f5fac6a181f0d290273049ea6593
luisgv3 <luisglezv3@gmail.com>
2024-09-17 22:12:56 +0000

    Changed return message

--- hello-world/app.js
+++ hello-world/app.js
@@ -18,11 +18,10 @@ exports.lambdaHandler = async (event, context) => {
     try {
         // const ret = await axios(url);
         response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
-            })
+          statusCode: 200,
+          body: JSON.stringify({
+            message: "I'm using canary deployments",
+          }),
         }
     } catch (err) {
         console.log(err);
